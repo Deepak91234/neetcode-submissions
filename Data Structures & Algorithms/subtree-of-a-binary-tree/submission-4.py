ecb86b1fcb9106100aae1bfa161ab94a03e48722
Deepak91234 <deepakandi123@gmail.com>
2026-09-10 16:04:39 -0400

    Add: subtree-of-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        def isSameTree(p, q):
+            if not p and not q:
+                return True
+
+            if not p or not q:
+                return False
+
+            if p.val != q.val:
+                return False
+
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+        if not subRoot:
+            return True
+
+        if not root:
+            return False
+
+        if isSameTree(root, subRoot):
+            return True
+
+        if self.isSubtree(root.left, subRoot):
+            return True
+
+        if self.isSubtree(root.right, subRoot):
+            return True
+
+        return False
